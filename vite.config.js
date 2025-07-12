87033f0853cbc7a1a5645f7a432d7ca4510f59b2
kimsohea <kimsohea4425@gmail.com>
2025-07-12 21:48:24 +0900

    build: scss 추가 및 git page deploy

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import { fileURLToPath, URL } from "node:url";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/code-experience/",
   plugins: [react()],
   server: {
     port: 8080,
