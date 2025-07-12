8dda12b3f798709466976cf03e909df812875479
kimsohea <kimsohea4425@gmail.com>
2025-07-12 21:37:20 +0900

    init: 프로젝트 생성

---
+++ vite.config.js
@@ -0,0 +1,24 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { fileURLToPath, URL } from "node:url";
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    port: 8080,
+    proxy: {
+      "/api": {
+        target: "https://apis.data.go.kr/6270000/getTourKorAttract",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
+    },
+  },
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+      components: fileURLToPath(new URL("./src/components", import.meta.url)),
+    },
+  },
+});
